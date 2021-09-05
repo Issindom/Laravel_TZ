4c1de180f2f7c17ebb88e37396fa88b1ec81df2d
DESKTOP-Q5P1EVF\PC <a.samarcew@yandex.ru>
2021-09-05 12:36:58 +0400

    миграции, модели, контроллеры

---
+++ database/migrations/2021_09_05_082118_create_users_authors.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersAuthors extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('fio');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
